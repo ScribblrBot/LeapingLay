9da65e8f36bdbde91bf3fcf1f7893e457a7b31b6
ScribblrBot <scribblrbot@gmail.com>
2025-09-08 01:58:30 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 // pages/_app.js
+import Head from 'next/head';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/img/5c753379-3cb1-4c2a-bf4b-2310f8c89d6e.png" />
+        <title>LeapingLay - Profile</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
