1e2f95d7df517143f2debda9940ee502c0285626
ScribblrBot <scribblrbot@gmail.com>
2025-09-08 13:19:19 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function ProfilePage() {
       "handle": "leapinglay",
       "pronouns": "He/Him",
       "profile": {
-        "pfp": "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2864&q=80"
+        "pfp": "/img/5c753379-3cb1-4c2a-bf4b-2310f8c89d6e.png"
       },
       "socials": {
         "youtube": "UC7mh6hOpjyMnSpwZ8Nr_h-Q",
