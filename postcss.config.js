b22b40eab04f7af717343e8175020f9509ea0be8
ScribblrBot <scribblrbot@gmail.com>
2025-09-08 01:33:12 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   plugins: {
-    'postcss-flexbugs-fixes': {},
-    'postcss-preset-env': {
-      stage: 3,
-      features: {
-        'nesting-rules': true
-      }
-    },
     tailwindcss: {},
     autoprefixer: {},
-  }
+  },
 }
